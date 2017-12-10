9495685fc89f183ce6c358e2b5db909505fd4243
MoraMaz <lestermaz.96@gmail.com>
2017-12-10 03:17:16 -0600

    Matriz
    Implementación de la matriz terminada

--- Practica1_201403610/Practica1_201403610/Estructuras/Matriz.cs
+++ Practica1_201403610/Practica1_201403610/Estructuras/Matriz.cs
@@ -5,35 +5,86 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Practica1_201403610.Estructuras
-{
+{    
+    /* RECORDATORIO DE LA MATRIZ
+     * Crear una matriz "a" de 2 filas por 3 columnas:
+     * Estructuras.Matriz a = new Estructuras.Matriz(2, 3);
+     * 
+     * * El método constructor de la matriz recibe dos enteros: 
+     *   cantidad de filas y cantidad de columnas, en ese orden.
+     *      NOTA:
+     *          cantidad de filas = tamaño en y
+     *          cantidad de columnas = tamaño en x *
+     * 
+     * - Posiciones disponibles para el caso de esta matriz:
+     * - (0,0) (0,1) (0,2) (1,0) (1,1) (1,2)
+     * 
+     * Insertar los números del 0 al 2 en la primera fila (fila 0):
+     * a.set(0, 0, 0);
+     * a.set(1, 0, 1);
+     * a.set(2, 0, 2);
+     * 
+     * Insertar los números del 3 al 5 en la segunda fila (fila 1):
+     * a.set(3, 1, 0);
+     * a.set(4, 1, 1);
+     * a.set(5, 1, 2);
+     * 
+     * * El método set de la matriz recibe tres enteros: dato, 
+     *   fila y columna, en ese orden.
+     *      NOTA: 
+     *          fila = posición en y
+     *          columna = posicion en x *
+     */
+
     class Matriz
     {
         private NodoMatriz inicio;
-        private int x, y;
+        private int filas, columnas;
 
-        public Matriz(int x, int y)
+        public Matriz(int filas, int columnas)
         {
             inicio = new NodoMatriz(0, 0, 0);
-            this.x = x;
-            this.y = y;
+            this.columnas = columnas;
+            this.filas = filas;
+            generarMatriz();
         }
 
         private void generarMatriz()
         {
-            int j;
-            NodoMatriz anterior, actual;
-            j = y;
+            int i;
+            NodoMatriz anterior, actual, aux1, aux2;
+            j = filas - 1;
             crearFila(inicio, 0);
             anterior = null;
             actual = inicio;
-            while (j > 0)
+            while (i > 0)
             {
                 actual.Abajo = new NodoMatriz(0, actual.getPosition()[0], actual.getPosition()[1] + 1);
                 anterior = actual;
                 actual = actual.Abajo;
                 actual.Arriba = anterior;
                 crearFila(actual, actual.getPosition()[1]);
-                j--;
+                i--;
+            }
+            anterior = inicio;
+            actual = inicio.Abajo;
+            while (actual != null)
+            {
+                aux1 = anterior;
+                aux2 = actual;
+                for (i = 0; i < columnas - 1; i++)
+                {
+                    aux1 = aux1.Derecha;
+                    aux2 = aux2.Derecha;
+                    if (aux1 != null && aux2 != null)
+                    {
+                        aux1.Abajo = aux2;
+                        aux2.Arriba = aux1;
+                    }
+
+                }
+                anterior = actual;
+                actual = actual.Abajo;
             }
         }
 
@@ -41,7 +92,7 @@ namespace Practica1_201403610.Estructuras
         {
             int i;
             NodoMatriz anterior = null;
-            for (i = 0; i < x - 1; i++)
+            for (i = 0; i < columnas - 1; i++)
             {
                 actual.Derecha = new NodoMatriz(0, i + 1, fila);
                 anterior = actual;
@@ -49,6 +100,33 @@ namespace Practica1_201403610.Estructuras
                 actual.Izquierda = anterior;
             }
             actual.Izquierda = anterior;
+        }
+
+        private NodoMatriz get(int fila, int columna)
+        {
+            int i, j;
+            NodoMatriz buscado = inicio;
+            for (i = 0; i < this.columnas; i++)
+            {
+                if (columna == i)
+                    for (j = 0; j < this.filas; j++)
+                    {
+                        if (fila == j)
+                            return buscado;
+                        buscado = buscado.Abajo;
+                    }
+                buscado = buscado.Derecha;
+            }
+            return null;
+        }
+
+        public void set(int dato, int fila, int columna)
+        {
+            if (columna >= this.columnas || fila >= this.filas)
+                return;
+            NodoMatriz seteado = get(fila, columna);
+            if (seteado != null)
+                seteado.Dato = dato;
         }
 
     }
